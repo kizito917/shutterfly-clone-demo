57c5b3069ff4be8a24f5f02cbf540d57540b90d6
kizito917 <johnkingsley917@gmail.com>
2025-04-30 10:30:58 +0100

    Job poll log introduced.

--- shutterfly-clone-frontend/src/utils/poll.js
+++ shutterfly-clone-frontend/src/utils/poll.js
@@ -14,6 +14,7 @@ export const poll = async (
   const pollJob = async (attempt = 0) => {
     const delayMs = exponentialDelay(attempt);
     const statusResult = await job();
+    console.log("STATUS RESULT FROM JOB+++++++", statusResult)
 
     const status = statusResult.job.status.toLocaleLowerCase();
 
